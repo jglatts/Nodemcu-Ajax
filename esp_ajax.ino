2138ccbb88772af0fcd4b69c9efb2cccac8eaaf1
John Glatts <33476958+jglatts@users.noreply.github.com>
2019-04-08 20:37:33 -0400

    Update esp_ajax.ino

--- esp_ajax.ino
+++ esp_ajax.ino
@@ -5,25 +5,26 @@
  * Date: 4/6/19
  *
  * ToDo:
- *      - Add a .h file and place all the html stuff there
- *
+ *      - :)
  */
 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+// JS-HTML file
+#include "htmlpage.h"
 
 
 // Replace with your network credentials
-const char* ssid = "your-wifi";
-const char* password = "your-wifi";
+const char* ssid = "your-network";
+const char* password = "your-network";
 ESP8266WebServer server(80);   //instantiate server at port 80 (http port)
 
 
-String page = "";
-String time_page = "";
 String text = "";
 String time_text = "";
+String calc_time_page = "";
+String index_page = "";
 unsigned long data, finish_time, elapsed_time, mod, motor_mins, motor_secs;
 
 
@@ -63,26 +64,8 @@ void setup(void){
     });
     server.on("/Time", calcTime);
     server.on("/", [](){
-        page = "<h1>Sensor to Node MCU Web Server</h1>\r\n";
-        // change the JS here to record the time ONLY when the Time It link has been clicked
-        page += "<br><a href=\"Time\" onclick=\"\">Time It</a>\r\n";
-        page += "<script>\r\n";
-        page += "var x = setInterval(function() {loadData(\"data.txt\",updateData)}, 1000);\r\n";
-        page += "function loadData(url, callback){\r\n";
-        page += "var xhttp = new XMLHttpRequest();\r\n";
-        page += "xhttp.onreadystatechange = function(){\r\n";
-        page += " if(this.readyState == 4 && this.status == 200){\r\n";
-        page += " callback.apply(xhttp);\r\n";
-        page += " }\r\n";
-        page += "};\r\n";
-        page += "xhttp.open(\"GET\", url, true);\r\n";
-        page += "xhttp.send();\r\n";
-        page += "}\r\n";
-        page += "function updateData(){\r\n";
-        page += " console.log(this.responseText);\r\n";
-        page += "}\r\n";
-        page += "</script>\r\n";
-        server.send(200, "text/html", page);
+        index_page = page;
+        server.send(200, "text/html", index_page);
     });
 
     server.begin();
@@ -91,36 +74,11 @@ void setup(void){
 
 
 void calcTime() {
-    time_page = "<h1>Sensor to Node MCU Web Server</h1>";
-    time_page += "<h1 style=\"display:inline;\">Time: </h1><h1 id=\"mins\" style=\"display:inline;\">""</h1><h1 style=\"display:inline;\"> mins </h1>";
-    time_page += "<h1 style=\"display:inline;\"> and </h1><h1 id=\"secs\" style=\"display:inline;\">""</h1>";
-    time_page += "<h1 style=\"display:inline;\"> secs. </h1>\r\n";
-    time_page += "<script>\r\n";
-    time_page += "var x = setInterval(function() {loadTime(\"time.txt\",updateTime)}, 1000);\r\n";
-    time_page += "function loadTime(url, callback){\r\n";
-    time_page += "var xhttp_time = new XMLHttpRequest();\r\n";
-    time_page += "xhttp_time.onreadystatechange = function(){\r\n";
-    time_page += " if(this.readyState == 4 && this.status == 200){\r\n";
-    time_page += " callback.apply(xhttp_time);\r\n";
-    time_page += " }\r\n";
-    time_page += "};\r\n";
-    time_page += "xhttp_time.open(\"GET\", url, true);\r\n";
-    time_page += "xhttp_time.send();\r\n";
-    time_page += "}\r\n";
-    time_page += "function updateTime(){\r\n";
-    time_page += "var timeArray = this.responseText.split(\"||\");\r\n";
-    time_page += "console.log(timeArray[0]);\r\n";
-    time_page += "console.log(timeArray[1]);\r\n";
-    time_page += "console.log(\"----------- \");\r\n";
-    time_page += "document.getElementById(\"mins\").innerHTML = timeArray[0];\r\n";
-    time_page += "document.getElementById(\"secs\").innerHTML = timeArray[1];\r\n";
-    time_page += "}\r\n";
-    time_page += "</script>\r\n";
-    server.send(200, "text/html", time_page);
+    calc_time_page = time_page;
+    server.send(200, "text/html", calc_time_page);
 }
 
 
 void loop(void){
-    delay(1000);
     server.handleClient();
 }
